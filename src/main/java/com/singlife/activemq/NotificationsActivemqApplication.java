be11f99b668c5361f29c698f19a2730c67cddf07
akilas94 <akilasilva94@gmail.com>
2022-11-13 10:51:48 +0800

    init commit

---
+++ src/main/java/com/singlife/activemq/NotificationsActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.singlife.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsActivemqApplication.class, args);
+	}
+
+}
